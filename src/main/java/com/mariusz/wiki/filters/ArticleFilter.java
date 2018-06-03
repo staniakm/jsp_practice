2ccedc286487e9c8f34609c7c38e76f956f2e835
Mariusz Staniak <mariusz.staniak@gmail.com>
2018-06-03 18:28:45 +0200

    add filters

---
+++ src/main/java/com/mariusz/wiki/filters/ArticleFilter.java
@@ -0,0 +1,34 @@
+package com.mariusz.wiki.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/wiki/main/articles/article/*","/wiki/main/articles/new/*"})
+public class ArticleFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("text/html; charset=UTF-8");
+        HttpSession session = ((HttpServletRequest) servletRequest).getSession();
+        Object auth = session.getAttribute("userAuth");
+        if (auth == null) {
+            RequestDispatcher dispatcher = servletRequest.getRequestDispatcher("/login");
+            dispatcher.forward(servletRequest, servletResponse);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/mariusz/wiki/filters/LogoutFilter.java
@@ -0,0 +1,34 @@
+package com.mariusz.wiki.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/logout")
+public class LogoutFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest) servletRequest).getSession();
+//        session.setAttribute("authUser",);
+        session.removeAttribute("userAuth");
+
+        ((HttpServletResponse)servletResponse).sendRedirect(((HttpServletRequest) servletRequest).getContextPath()+"/wiki/main");
+
+//        RequestDispatcher dispatcher = servletRequest.getRequestDispatcher("/wiki/main");
+//        dispatcher.forward(servletRequest,servletResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
